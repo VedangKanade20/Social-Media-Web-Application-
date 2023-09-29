3723ffe0fd23b54f584c32209feb2f42021f1ab3
VedangKanade <vedangk20@gmail.com>
2023-09-29 13:48:23 +0530

    Added a new model that is "POST"

--- server/index.js
+++ server/index.js
@@ -7,10 +7,13 @@ import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js ";
 import userRoutes from "./routes/users.js";
-import { fileURLToPath } from "url";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth";
 
 /* CONFIG */
 const __filename = fileURLToPath(import.meta.url);
@@ -41,10 +44,12 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP  */
 const PORT = process.env.PORT || 6001;
