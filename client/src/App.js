e8244199d94689cd7a6f0e65e8c10dc18a1d92a4
VedangKanade <vedangk20@gmail.com>
2023-10-09 00:27:11 +0530

    frontend continuation

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "scenes/homepage";
+import LoginPage from "scenes/loginpage";
+import ProfilePage from "scenes/profilepage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
