71a1cd601a50e08749829ef0f61f9f6c1a433c25
VedangKanade <vedangk20@gmail.com>
2023-10-31 18:29:37 +0530

    Backend

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -29,5 +29,19 @@ export const authSlice = createSlice({
         console.error("User friends non-existent :( ");
       }
     },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post._id) return action.payload.post;
+        return post;
+      });
+      state.posts = updatedPosts;
+    },
   },
 });
+
+export const { setFriends, setMode, setPost, setPosts, setLogout, setLogin } =
+  authSlice.actions;
+export default authSlice.reducer;
